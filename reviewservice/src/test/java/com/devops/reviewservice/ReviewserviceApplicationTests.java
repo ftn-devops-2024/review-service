665f2902753fe8d73f3b83bdc563a1624d308cd7
unknown <stefan.milosevic.e14@gmail.com>
2024-05-25 15:31:51 +0200

    Project init

---
+++ reviewservice/src/test/java/com/devops/reviewservice/ReviewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
