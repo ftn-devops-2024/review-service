4eb3e5c144d57d1f4c5400f65ff7e585fd3423aa
Ivana Kasikovic <58866167+kasikovicivana@users.noreply.github.com>
2024-06-02 15:17:09 +0200

    feat: add build and test pipeline (#3)
    * feat: add build and test pipeline
    
    * feat: configure test containers for tests
    
    * test: test with flaky tests
    
    * fix: remove flaky tests
    
    * fix: remove test branch

--- reviewservice/src/test/java/com/devops/reviewservice/ReviewserviceApplicationTests.java
+++ reviewservice/src/test/java/com/devops/reviewservice/ReviewserviceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.devops.reviewservice;
 
+import com.devops.reviewservice.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@WebMvcTest(TestController.class)
 class ReviewserviceApplicationTests {
 
 	@Test
