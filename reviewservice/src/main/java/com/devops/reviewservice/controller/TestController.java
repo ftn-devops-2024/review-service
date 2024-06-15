2b478eb7cc36ed09d1ebd9082269cfd33d646747
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-06-15 23:16:24 +0200

    Add prometheus and loki connected to grafana

--- reviewservice/src/main/java/com/devops/reviewservice/controller/TestController.java
+++ reviewservice/src/main/java/com/devops/reviewservice/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.devops.reviewservice.controller;
 
+import com.devops.reviewservice.service.ReviewService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class TestController {
 
+    Logger logger = LoggerFactory.getLogger(ReviewService.class);
     @GetMapping("/test")
     public String test(){
+        logger.info("ajde molim te");
         return "Welcome from review-service";
     }
 }
